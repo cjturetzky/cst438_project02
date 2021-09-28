a0f054218366413c884019fd2fd4dcf7bc4d648e
thekokoloko <sotoramiro30@yahoo.com>
2021-09-28 11:01:42 -0700

    setting up path functions

--- cst438_project02/views.py
+++ cst438_project02/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # create your views here
+import cst438_project02
+
 
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'static/LandingPage.html')
 
 def products(request):
     return HttpResponse('products')
