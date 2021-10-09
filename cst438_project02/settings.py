f916ada45158082c9eba287f9f6a2788b31b2901
thekokoloko <sotoramiro30@yahoo.com>
2021-10-08 18:31:37 -0700

    Launch Website with Heroku commit

--- cst438_project02/settings.py
+++ cst438_project02/settings.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
+import os
+from pathlib import Path
+
 import os.path
 from pathlib import Path
 import django_heroku
@@ -122,11 +126,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 #ok
-STATIC_URL = '/static/'
-STATIC_ROOT = "/var/www/example.com/static/"
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATIC_URL = '/static/'
+# STATIC_ROOT = "/var/www/example.com/static/"
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
