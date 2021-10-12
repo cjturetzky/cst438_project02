ae72965284282e6088edb2899aa84820cb18e0f6
thekokoloko <sotoramiro30@yahoo.com>
2021-10-12 04:13:13 -0700

    Launch Website with Heroku commit

--- cst438_project02/urls.py
+++ cst438_project02/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('', include('wishlist.urls')),
     # path('',views.home),
     # path('products/', views.products),
     # path('customer/', views.customer),
--- main/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Users, WishlistItem
-
-# Register your models here.
-admin.site.register(Users);
-admin.site.register(WishlistItem);
