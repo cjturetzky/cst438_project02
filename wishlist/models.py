3f31006d60a77795eaa411e5c135f1fafc6b5c13
JeremyMeh <jeremymm125@gmail.com>
2021-10-17 20:13:30 -0700

    This is adding the create account functionality from Ramiro's code with my own.

--- wishlist/models.py
+++ wishlist/models.py
@@ -4,8 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Users(models.Model):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
 
     def __str__(self):
         return self.username
