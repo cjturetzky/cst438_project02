b516e9c0f304abcdbaaf2991ad5706a05c5fc6d9
thekokoloko <sotoramiro30@yahoo.com>
2021-10-19 10:20:12 -0700

    Edit user profile page commit

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+from .views import (WishListView,
+                    WishDetailView,
+                    WishCreateView
+)
+
 #import views
 
 urlpatterns = [
-    path('', views.home, name='wishlist-home'),
+    path('', WishListView.as_view(), name='wishlist-home'),
+    path('wish/<int:pk>', WishDetailView.as_view(), name='wishlist-detail'),
+    path('wish/new/', WishCreateView.as_view(), name='wishlist-create'),
     path('about/', views.about, name='wishlist-about'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from .models import Wish
+#import django class base views, import detail view
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView
+)
+#add mixin to prevent creation of wish if your are not logged in
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 
 posts = [
@@ -23,6 +32,30 @@ def home(request):
         'posts': Wish.objects.all()
     }
     return render(request, 'wishlist/LandingPage.html', context)
+
+class WishListView(ListView):
+    model = Wish
+    template_name = 'wishlist/LandingPage.html'
+    #set wish variable to list objects
+    context_object_name = 'posts'
+    #order list from older to newest
+    ordering = ['-date_posted']
+
+class WishDetailView(DetailView):
+    model = Wish
+
+class WishCreateView(LoginRequiredMixin, CreateView):
+    model = Wish
+    fields = ['title', 'content']
+
+    #form valid method overwrite
+    def form_valid(self, form):
+        #set author to current logged in user
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+
 
 def about(request):
     return render(request, 'wishlist/about.html', {'title': 'About'})
