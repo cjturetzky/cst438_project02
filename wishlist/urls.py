9d4e48b7f0718ff9daea1e898c327dd4054d930f
Jose Caudillo <caudillojose5@gmail.com>
2021-10-20 22:20:27 -0700

    connected list view html to backend

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.home, name='wishlist-home'),
     path('about/', views.about, name='wishlist-about'),
     # path('createaccount/', views.createAccount, name='wishlist-createaccount'),
+    
 ]
